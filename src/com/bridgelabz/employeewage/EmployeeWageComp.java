8c2d83dc1641c405493deda8646e83c9aabac320
Textspot <sachinkatarki1995@gmail.com>
2021-12-22 13:22:47 +0530

    Added:welcome to the Employeewage Programme

---
+++ src/com/bridgelabz/employeewage/EmployeeWageComp.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWageComp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeeWage Computation Programme");
+
+	}
+
+}
