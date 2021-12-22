71b34802b98a7d5ba88bfc1fbc1589309a5e91af
Textspot <sachinkatarki1995@gmail.com>
2021-12-22 13:32:57 +0530

    Added:Need to check the Employees Attendance Present or Absent

--- src/com/bridgelabz/employeewage/EmployeeWageComp.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComp.java
@@ -4,6 +4,14 @@ public class EmployeeWageComp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to EmployeeWage Computation Programme");
+		int is_present = 1;
+		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
+		if( emp_check == is_present)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+			System.out.println("Employee is absent");
 
 	}
 
