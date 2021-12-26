97245cc8186573005bfbb7eac7d3b10f2289358c
Sachin2022 <96484498+Sachin2022@users.noreply.github.com>
2021-12-26 10:19:32 +0530

    Update EmployeeWageComp.java

--- src/com/bridgelabz/employeewage/EmployeeWageComp.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComp.java
@@ -14,24 +14,24 @@ public class EmployeeWageComp {
 		for (int i = 0; i < days_in_month; i++) {
 			int emp_check = (int) (Math.floor(Math.random() * 10)) % 3;
 			switch (emp_check) {
-			case 1:
-				working_hr = 8;
-				break;
-			case 2:
-				working_hr = 4;
-				break;
-			default:
-				working_hr = 0;
-			}
+			case 1:working_hr = 8;
+			 break;	
+				
+			case 2:working_hr = 4;
+			 break;	
+				
+			default:working_hr = 0;
+				
+		    }
 			daily_wage_array[i] = working_hr * wage_per_hr;
-		}
+		    }
 
-		for (int j = 0; j < days_in_month; j++) {
+		        for (int j = 0; j < days_in_month; j++) {
 			int day = j + 1;
 			System.out.println("Day " + day + " wage is " + daily_wage_array[j]);
 			monthly_total_wage = monthly_total_wage + daily_wage_array[j];
-		}
-		System.out.println("\nMonthly wage is " + monthly_total_wage);
-	}
+		   }
+		        System.out.println("\nMonthly wage is " + monthly_total_wage);
+	        }
 
-}
+              }
