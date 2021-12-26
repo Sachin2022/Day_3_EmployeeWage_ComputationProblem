3d73b97b7b54e6cb2afac1facdf64eca90631f9c
Sachin2022 <96484498+Sachin2022@users.noreply.github.com>
2021-12-26 10:11:30 +0530

    Update EmployeeWageComp.java

--- src/com/bridgelabz/employeewage/EmployeeWageComp.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComp.java
@@ -16,24 +16,24 @@ public class EmployeeWageComp {
 		for (int i=0; i<days_in_month && total_working_hr<max_working_hrs; i++) {
 			int emp_check = (int)(Math.floor(Math.random() * 10)) % 3;
 			switch (emp_check) {
-				case 1:
-					working_hr = 8;
-					break;
-				case 2:
-					working_hr = 4;
-					break;
+				case 1:working_hr = 8;
+				 break;	
+					
+				case 2:working_hr = 4;
+				 break;	
+					
 				default:
 					working_hr = 0;
-			}
-			total_working_hr += working_hr;
-			daily_wage_array[i] = working_hr * wage_per_hr;
-		}
-		for (int j=0; j<days_in_month; j++) {
-			int day = j+1;
-			System.out.println("Day " + day + " wage is " + daily_wage_array[j]);
-			monthly_total_wage = monthly_total_wage + daily_wage_array[j];
-		}
-		System.out.println("\nMonthly wage is " + monthly_total_wage);
-	}
+		  }
+			     total_working_hr += working_hr;
+			     daily_wage_array[i] = working_hr * wage_per_hr;
+		  }
+		       for (int j=0; j<days_in_month; j++) {
+			  int day = j+1;
+			  System.out.println("Day " + day + " wage is " + daily_wage_array[j]);
+			  monthly_total_wage = monthly_total_wage + daily_wage_array[j];
+		 }
+		      System.out.println("\nMonthly wage is " + monthly_total_wage);
+	    }
 
-}
+       }
